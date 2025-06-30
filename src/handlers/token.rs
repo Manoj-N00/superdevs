8c394eeeebe57d717367c5ee6bd7a8fefdd841e8
Manoj-N00 <manojnaikj@gmail.com>
2025-06-30 23:57:04 +0530

    init

--- src/handlers/token.rs
+++ src/handlers/token.rs
@@ -1,6 +1,7 @@
 use actix_web::{post, web, HttpResponse};
 use solana_program::{system_program, pubkey::Pubkey};
 use solana_sdk::signer::Signer;
+use base64::Engine;
 use spl_token::instruction as token_instruction;
 use crate::{
     models::{ApiResponse, CreateTokenRequest, MintTokenRequest, InstructionResponse, AccountMeta},
@@ -30,7 +31,7 @@ pub async fn create_token(req: web::Json<CreateTokenRequest>) -> ServerResult<Ht
             is_signer: acc.is_signer,
             is_writable: acc.is_writable,
         }).collect(),
-        instruction_data: base64::encode(instruction.data),
+        instruction_data: base64::engine::general_purpose::STANDARD.encode(&instruction.data),
     };
 
     Ok(HttpResponse::Ok().json(ApiResponse::success(response)))
@@ -63,7 +64,7 @@ pub async fn mint_token(req: web::Json<MintTokenRequest>) -> ServerResult<HttpRe
             is_signer: acc.is_signer,
             is_writable: acc.is_writable,
         }).collect(),
-        instruction_data: base64::encode(instruction.data),
+        instruction_data: base64::engine::general_purpose::STANDARD.encode(&instruction.data),
     };
 
     Ok(HttpResponse::Ok().json(ApiResponse::success(response)))
